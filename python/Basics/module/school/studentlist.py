8a08acdfcc5748167517ed8ec55df7d6d238cb36
ploradoaa <dideoeo555@gmail.com>
2024-03-16 01:34:47 +0900

    add files

--- python/Basics/module/school/studentlist.py
+++
@@ -1,8 +0,0 @@
-class StudentList:
-    def __init__(self):
-        self.__list = []
-    def append(self, 학생):
-        self.__list.append(학생)
-    def print(self):
-        for student in self.__list:
-            print(student.sum())
